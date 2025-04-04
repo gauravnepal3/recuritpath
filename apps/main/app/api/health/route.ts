166c6c6358f37264e9acce21936ab66d8d25c0dd
Gaurav Nepal <gauravnepal3@gmail.com>
2025-04-04 15:53:55 +0545

    Health Check added

--- apps/main/app/api/health/route.ts
+++ apps/main/app/api/health/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
     try {
-        NextResponse.json({ message: "Running" }, { status: 200 });
+        return NextResponse.json({ message: "Running" }, { status: 200 });
     } catch (error) {
         console.error("Error generating pre-signed URL:", error);
-        NextResponse.json({ error: "Failed to run health check" }, { status: 500 });
+        return NextResponse.json({ error: "Failed to run health check" }, { status: 500 });
     }
 }
