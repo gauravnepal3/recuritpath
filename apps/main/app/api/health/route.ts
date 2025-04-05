c16dc59797d4a95b1b3ab2bb33e33d5299c2b4cf
Gaurav Nepal <gauravnepal3@gmail.com>
2025-04-05 18:38:12 +0545

    Change

--- apps/main/app/api/health/route.ts
+++ apps/main/app/api/health/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@repo/database"
-export async function GET(req: NextRequest) {
-    try {
-        const organization = await prisma.organization.findMany({
-            where: {
+import { prisma } from "@repo/database";
 
-            }
-        });
-        return NextResponse.json({ message: "Running" }, { status: 200 });
+export async function GET(req: NextRequest) {
+    const shouldSkipPrisma = process.env.DISABLE_PRISMA === "true";
+
+    try {
+        if (!shouldSkipPrisma) {
+            const organizations = await prisma.organization.findMany();
+            return NextResponse.json({ message: "DB connection successful", organizations }, { status: 200 });
+        } else {
+            return NextResponse.json({ message: "Prisma connection skipped" }, { status: 200 });
+        }
     } catch (error) {
-        console.error("Error generating pre-signed URL:", error);
+        console.error("Prisma error in health check:", error instanceof Error ? error.stack : error);
         return NextResponse.json({ error: "Failed to run health check" }, { status: 500 });
     }
 }
