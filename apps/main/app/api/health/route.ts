4751d72184781f5700bc55744e03b34d39e7be86
Gaurav Nepal <gauravnepal3@gmail.com>
2025-04-05 09:07:48 +0545

    test: Database Connection

--- apps/main/app/api/health/route.ts
+++ apps/main/app/api/health/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { prisma } from "@repo/database"
 export async function GET(req: NextRequest) {
     try {
+        const organization = await prisma.organization.findMany();
         return NextResponse.json({ message: "Running" }, { status: 200 });
     } catch (error) {
         console.error("Error generating pre-signed URL:", error);
